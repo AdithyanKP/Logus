2bdb8c993129d34b96c17f020d121a30bde6579c
Adithyan k P <adithyankp777@gmail.com>
2022-03-30 15:12:57 +0530

    app bar implemetation

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
 import React from "react";
-
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
+import memories from "./images/image.png";
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>helllo</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img className={classes.image} src={memories} alt="logo" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            conatiner
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./post/Post";
+
+const Posts = () => {
+  return (
+    <>
+      <h1>Posts</h1>
+      <Post />
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
