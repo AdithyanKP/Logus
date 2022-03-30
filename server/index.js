20f1829545cd8065ede1c4df2bf1671d76f7e3b7
Adithyan k P <adithyankp777@gmail.com>
2022-03-30 12:22:02 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -2,3 +2,31 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+//route use
+
+app.use("/posts", postRoutes);
+
+//basic usage
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//mongoose connection
+
+const CONNECTION_URL =
+  "mongodb+srv://adithyankp:adithyankp123@cluster0.wu2ak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on Port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+//port setup
+const PORT = process.env.PORT || 5000;
---
+++ server/routes/posts.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+import { getPosts, createPosts } from "../controllers/posts.js";
+const router = express.Router();
+
+router.get("/", getPosts);
+router.post("/", createPosts);
+export default router;
