a9895d73a52ebf9d853719ab43c733c7d23b9a63
Adithyan k P <adithyankp777@gmail.com>
2022-04-22 17:38:40 +0530

    change

--- client/src/components/Posts/post/Post.js
+++ client/src/components/Posts/post/Post.js
@@ -100,13 +100,17 @@ const Post = ({ post, currentId, setCurrentId }) => {
           >
             <Likes />
           </Button>
-          <Button
-            size="small"
-            color="secondary"
-            onClick={() => dispatch(deletePost(post._id))}
-          >
-            <DeleteIcon fontSize="small" /> Delete
-          </Button>
+
+          {(user?.result?.googleId === post.creator ||
+            user?.result?._id === post.creator) && (
+            <Button
+              size="small"
+              color="secondary"
+              onClick={() => dispatch(deletePost(post._id))}
+            >
+              <DeleteIcon fontSize="small" /> Delete
+            </Button>
+          )}
         </CardActions>
       </Card>
     </>
